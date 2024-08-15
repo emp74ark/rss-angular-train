edc0abd158d178e664d51b58e9e41902ca2cb032
Andrei Yurkouski <emp74ark@gmail.com>
2024-08-16 00:44:10 +0200

    feat: auth guard

---
+++ src/app/services/auth.guard.ts
@@ -0,0 +1,15 @@
+import { CanMatchFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthService } from './auth.service';
+
+export const authGuard: CanMatchFn = () => {
+  const authToken = inject(AuthService).$authStatus.getValue().token;
+  const router = inject(Router);
+
+  if (authToken) {
+    return true;
+  } else {
+    // todo: replace route with the correct one
+    return router.createUrlTree(['/auth']);
+  }
+};
