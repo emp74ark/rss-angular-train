59c4a5ed2b868da1a886c5aecabc5588935a8f91
Andrei Yurkouski <emp74ark@gmail.com>
2024-08-17 14:44:23 +0200

    feat: disable auth routes for authenticated user in the auth guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,15 +1,27 @@
-import { CanMatchFn, Router } from '@angular/router';
+import { CanMatchFn, Route, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { LogService } from '../services/log.service';
 
-export const authGuard: CanMatchFn = () => {
+export const authGuard: CanMatchFn = (route: Route) => {
   const authToken = inject(AuthService).$authStatus.getValue().token;
   const router = inject(Router);
+  const logService = inject(LogService);
+
+  if (!authToken && route.path?.startsWith('auth')) {
+    logService.logMessage('auth route enabled for non-authenticated', 'info', 'auth guard');
+    return true
+  }
+
+  if (authToken && route.path?.startsWith('auth')) {
+    logService.logMessage('auth route disabled for authenticated', 'info', 'auth guard');
+    return router.createUrlTree(['/']);
+  }
 
   if (authToken) {
+    logService.logMessage(`${route.path} route enabled for authenticated`, 'info', 'auth guard');
     return true;
-  } else {
-    // todo: replace route with the correct one
-    return router.createUrlTree(['/auth']);
   }
+
+  return router.createUrlTree(['/auth']);
 };
