a36ad9ec0e0c060858bc839b3c9b2d8cc592430c
Andrei Yurkouski <andrei.yurkouski@rapidfunnel.com>
2024-08-18 12:16:58 +0200

    fix: update services

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,26 +2,30 @@ import { CanMatchFn, Route, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { LogService } from '../services/log.service';
+import { map } from 'rxjs';
 
 export const authGuard: CanMatchFn = (route: Route) => {
-  const authToken = inject(AuthService).$authStatus.getValue().token;
   const router = inject(Router);
   const logService = inject(LogService);
 
-  if (!authToken && route.path?.startsWith('auth')) {
-    logService.logMessage('auth route enabled for non-authenticated', 'info', 'auth guard');
-    return true
-  }
+  return inject(AuthService).$authStatus.pipe(
+    map(({ token }) => {
+      if (!token && route.path?.startsWith('auth')) {
+        logService.logMessage('auth route enabled for non-authenticated', 'info', 'auth guard');
+        return true;
+      }
 
-  if (authToken && route.path?.startsWith('auth')) {
-    logService.logMessage('auth route disabled for authenticated', 'info', 'auth guard');
-    return router.createUrlTree(['/']);
-  }
+      if (token && route.path?.startsWith('auth')) {
+        logService.logMessage('auth route disabled for authenticated', 'info', 'auth guard');
+        return router.createUrlTree(['/']);
+      }
 
-  if (authToken) {
-    logService.logMessage(`${route.path} route enabled for authenticated`, 'info', 'auth guard');
-    return true;
-  }
+      if (token) {
+        logService.logMessage(`${route.path} route enabled for authenticated`, 'info', 'auth guard');
+        return true;
+      }
 
-  return router.createUrlTree(['/auth']);
+      return router.createUrlTree(['/auth']);
+    }),
+  );
 };
