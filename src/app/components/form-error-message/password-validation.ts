73b735d29767d85dcff790bff3da700ef6f173c0
Andrei Yurkouski <emp74ark@gmail.com>
2024-08-20 11:52:06 +0200

    feat: custom error component

---
+++ src/app/components/form-error-message/password-validation.ts
@@ -0,0 +1,33 @@
+import {
+  AbstractControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+} from '@angular/forms';
+
+export function passwordValidation(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+
+    if (control.value && control.value.length < 8) {
+      return { minlength: true };
+    }
+
+    return null;
+  }
+}
+
+export function passwordMismatch(data: object): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    if ('form' in data) {
+      const form = data.form as FormGroup;
+      const password = form.get('password')?.value
+      const confirmPassword = form.get('confirmPassword')?.value
+
+      if (control.value && password !== confirmPassword) {
+        return { mismatch: true };
+      }
+    }
+
+    return null;
+  }
+}
