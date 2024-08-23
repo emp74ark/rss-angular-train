47b064a9f8125dcc8ad186e775be73e7decfee5f
ayaki-coder <sj.a.yakimovich@gmail.com>
2024-08-23 21:56:43 +0300

    feat: add 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '**', component: NotFoundPageComponent }];
