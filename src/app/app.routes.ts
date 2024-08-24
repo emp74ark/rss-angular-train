5957bdedb9eb71c191a6b399805cc407a9b4588b
siviavio4ka <siviavio4ka@gmail.com>
2024-08-24 12:46:24 +0300

    feat: update route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'profile',
+        title: 'Profile',
+        loadComponent : () => import('./pages/user-profile/user-profile.component').then(m => m.UserProfileComponent)
+        // TODO: Add guards
+    },
+];
