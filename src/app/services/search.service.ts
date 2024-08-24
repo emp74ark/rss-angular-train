54efaae164446cce99fd1dc20d6b9cfd54c3ff73
Andrei Yurkouski <andrei.yurkouski@rapidfunnel.com>
2024-08-24 11:49:07 +0200

    feat: search service

---
+++ src/app/services/search.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { BehaviorSubject, catchError, of, tap } from 'rxjs';
+import { ApiStatus } from '../models/common';
+import { SearchResult } from '../models/search';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SearchService {
+  constructor(private httpClient: HttpClient) {}
+
+  private $$searchResults = new BehaviorSubject<SearchResult | null>(null);
+
+  $searchResults = this.$$searchResults.asObservable();
+
+  private $$apiStatus = new BehaviorSubject<ApiStatus>({
+    success: false,
+    error: null,
+  });
+
+  $apiStatus = this.$$apiStatus.asObservable();
+
+  search(searchParams: {
+    fromLatitude: number;
+    fromLongitude: number;
+    toLatitude: number;
+    toLongitude: number;
+    time?: number;
+  }) {
+    return this.httpClient
+      .get<SearchResult>('/api/search', {
+        params: searchParams,
+      })
+      .pipe(
+        tap(result => {
+          this.$$searchResults.next(result);
+          this.$$apiStatus.next({ success: true, error: null });
+        }),
+        catchError(({ error }: HttpErrorResponse) => {
+          this.$$searchResults.next(null);
+          this.$$apiStatus.next({ success: false, error: error.message });
+          return of(error);
+        }),
+      );
+  }
+}
