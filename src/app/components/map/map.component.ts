f71d8a7eebc09d21c0090984f67ad1c32a4250d9
Andrei Yurkouski <andrei.yurkouski@rapidfunnel.com>
2024-08-26 18:03:23 +0200

    feat: initial embedded map support

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,8 +1,6 @@
-import { Component, DestroyRef, inject, input, OnInit } from '@angular/core';
-import { GeocodingService } from '../../services/geocoding.service';
-import { switchMap, tap } from 'rxjs';
-import { takeUntilDestroyed, toObservable } from '@angular/core/rxjs-interop';
-import { SafeUrl } from '@angular/platform-browser';
+import { Component, inject, input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { environment } from '../../../environments/environment';
 import { GeoLocation } from '../../models/geo';
 
 @Component({
@@ -12,30 +10,29 @@ import { GeoLocation } from '../../models/geo';
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss',
 })
-export class MapComponent implements OnInit {
-  geoService = inject(GeocodingService);
+export class MapComponent implements OnInit, OnChanges {
+  sanitizer = inject(DomSanitizer);
 
-  destroyRef = inject(DestroyRef);
+  coordinates = input<GeoLocation>();
 
-  mapImageUrl: SafeUrl | undefined;
+  mapUrl: SafeResourceUrl | undefined;
 
-  coordinates = input<GeoLocation | undefined>();
-
-  private coordinates$ = toObservable(this.coordinates);
+  updateMapUrl() {
+    const defaultData = '53.893009, 27.567444';
+    const center = this.coordinates()?.lat ? `${this.coordinates()?.lng}, ${this.coordinates()?.lat}` : defaultData;
+    const baseUrl = 'https://www.google.com/maps/embed/v1/view';
+    const url = `${baseUrl}?key=${environment.geo_api_key}&center=${center}&zoom=10`;
+    this.mapUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
 
   ngOnInit() {
-    this.coordinates$
-      .pipe(
-        switchMap(data => {
-          return this.geoService.getMap(data).pipe(
-            tap(response => {
-              const blob = new Blob([response], { type: 'image/png' });
-              this.mapImageUrl = window.URL.createObjectURL(blob);
-            }),
-          );
-        }),
-        takeUntilDestroyed(this.destroyRef),
-      )
-      .subscribe();
+    this.updateMapUrl();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('INPUT VALUES', this.coordinates());
+    if (changes['coordinates']) {
+      this.updateMapUrl();
+    }
   }
 }
