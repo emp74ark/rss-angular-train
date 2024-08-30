1d91237b48c7bbe63b7b23c1ae747a93d00051c9
ayaki-coder <sj.a.yakimovich@gmail.com>
2024-08-30 10:39:52 +0300

    feat: add result component with mock data

--- src/app/components/search-result/search-result.component.ts
+++ src/app/components/search-result/search-result.component.ts
@@ -1,24 +1,47 @@
 import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { SearchService } from '../../services/search.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, DatePipe, NgClass } from '@angular/common';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+import { dates, tripInfo, TripResponse, UserTrip } from './result-mock';
+import { ResultCardComponent } from '../result-card/result-card.component';
 
 @Component({
   selector: 'app-search-result',
   standalone: true,
-  imports: [AsyncPipe, MatListModule, MatCardModule],
+  imports: [
+    AsyncPipe,
+    MatListModule,
+    MatCardModule,
+    MatTabsModule,
+    MatButtonModule,
+    DatePipe,
+    NgClass,
+    ResultCardComponent,
+  ],
   templateUrl: './search-result.component.html',
   styleUrl: './search-result.component.scss',
 })
 export class SearchResultComponent implements OnInit {
   readonly searchService = inject(SearchService);
   private readonly destroyRef = inject(DestroyRef);
+  protected dates = dates;
+  protected activeDate: string = this.dates[0];
+  protected trips: UserTrip[];
+  private response: TripResponse = tripInfo;
 
   ngOnInit(): void {
     this.searchService.$searchResults
       .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe(data => console.log('SearchResultComponent', { data }));
+
+    this.trips = this.response[this.activeDate];
+  }
+
+  renderTrips(): void {
+    this.trips = this.response[this.activeDate];
   }
 }
