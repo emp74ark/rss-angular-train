6b7e202ea63bcb19fa349cdcef82023507d3d9ea
Andrei Yurkouski <andrei.yurkouski@rapidfunnel.com>
2024-08-30 11:36:28 +0200

    fix: highlight user route

--- src/app/components/route-graph/route-graph.component.ts
+++ src/app/components/route-graph/route-graph.component.ts
@@ -1,19 +1,25 @@
 import { Component, computed, inject, input } from '@angular/core';
 import { StationsService } from '../../services/stations.service';
-import { AsyncPipe, DatePipe } from '@angular/common';
+import { AsyncPipe, DatePipe, NgClass } from '@angular/common';
 import { Route } from '../../models/route';
 
 @Component({
   selector: 'app-route-graph',
   standalone: true,
-  imports: [AsyncPipe, DatePipe],
+  imports: [AsyncPipe, DatePipe, NgClass],
   templateUrl: './route-graph.component.html',
   styleUrl: './route-graph.component.scss',
 })
 export class RouteGraphComponent {
   route = input<Route>();
   startStation = input<number>();
+  startStationIndex = computed(() => {
+    return this.route()?.path?.findIndex(id => id === this.startStation());
+  });
   lastStation = input<number>();
+  lastStationIndex = computed(() => {
+    return this.route()?.path?.findIndex(id => id === this.lastStation());
+  });
 
   stepsAmount = computed(() => {
     const length = this.route()?.path?.length;
@@ -33,5 +39,12 @@ export class RouteGraphComponent {
       return `${Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60))} min`;
     }
     return null;
+  }
+
+  isHighlighted(index: number): boolean {
+    const start = this.startStationIndex();
+    const end = this.lastStationIndex();
+    if (!start || !end) return false;
+    return index >= start && index <= end;
   }
 }
