365b8bc5dcb7e3e35c5d0de63c11e479ea56b3f6
Andrei Yurkouski <andrei.yurkouski@rapidfunnel.com>
2024-09-02 00:33:55 +0200

    feat: use trip service

--- src/app/pages/trip-page/trip-page.component.ts
+++ src/app/pages/trip-page/trip-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { combineLatest, filter, map, switchMap, tap } from 'rxjs';
+import { combineLatest, filter, map, of, switchMap } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { SearchService } from '../../services/search.service';
 import { DatePipe, Location } from '@angular/common';
@@ -12,9 +12,9 @@ import { MatCard, MatCardActions, MatCardContent, MatCardTitle } from '@angular/
 import { ModalWindowComponent } from '../../components/modal-window/modal-window.component';
 import { RouteGraphComponent } from '../../components/route-graph/route-graph.component';
 import { parseInt } from 'lodash';
-import { CarriageService } from '../../services/carriage';
-import { CarriageData } from '../../models/carriage';
 import { ExtendedRoute } from '../../models/route';
+import { RideService } from '../../services/ride.service';
+import { DetailedRideInfo } from '../../models/train';
 
 @Component({
   selector: 'app-trip-page',
@@ -40,86 +40,60 @@ export class TripPageComponent implements OnInit {
   route = inject(ActivatedRoute);
   searchService = inject(SearchService);
   stationsService = inject(StationsService);
-  carriageService = inject(CarriageService);
+  rideService = inject(RideService);
   destroyRef = inject(DestroyRef);
 
-  rideId: number;
   tripRoute: ExtendedRoute;
-  stationFrom: { stationId: number; cityName: string };
-  departureDate: string;
-  stationTo: { stationId: number; cityName: string };
-  arrivalDate: string;
-  carriages: CarriageData[];
-  carriageTypes: CarriageData[];
+  stationFrom: { id: number; city: string };
+  stationTo: { id: number; city: string };
+  trainDetails: DetailedRideInfo;
 
-  getCarriageData(carriageName: string) {
-    return this.carriageTypes.find(el => el.code === carriageName);
+  getData() {
+    combineLatest([
+      this.route.params.pipe(
+        filter(params => params['id']),
+        map(params => parseInt(params['id'])),
+      ),
+      this.route.queryParams.pipe(
+        filter(params => params['from'] && params['to']),
+        map(params => [parseInt(params['from']), parseInt(params['to'])]),
+      ),
+    ])
+      .pipe(
+        map(([id, [from, to]]) => {
+          return { id, from, to };
+        }),
+        switchMap(({ id, from, to }) => {
+          return combineLatest([
+            of({ id, from, to }), // prev data
+            this.searchService.searchRide(id), // route
+            this.stationsService.getStationById(from), // stationFrom
+            this.stationsService.getStationById(to), // stationTo
+          ]);
+        }),
+        switchMap(([base, route, stationFrom, stationTo]) => {
+          const details = this.rideService.getDetailedInfo(
+            route,
+            route.rideId,
+            route.schedule.segments,
+            base.from,
+            base.to,
+          );
+          return of({ base, route, details, stationFrom, stationTo });
+        }),
+        takeUntilDestroyed(this.destroyRef),
+      )
+      .subscribe(result => {
+        console.log('RESULT', result);
+        this.tripRoute = result.route;
+        this.stationFrom = result.stationFrom;
+        this.stationTo = result.stationTo;
+        this.trainDetails = result.details;
+      });
   }
 
   ngOnInit() {
-    // preload stations
-    this.stationsService
-      .getStations()
-      .pipe(
-        // tap(st => console.log(st)),
-        takeUntilDestroyed(this.destroyRef),
-      )
-      .subscribe();
-    // preload carriage types
-    this.carriageService
-      .get()
-      .pipe(
-        switchMap(() => {
-          return this.carriageService.$carriages;
-        }),
-        tap(res => {
-          console.log(res);
-          this.carriageTypes = res;
-        }),
-        takeUntilDestroyed(this.destroyRef),
-      )
-      .subscribe();
-    // id
-    this.route.params
-      .pipe(
-        tap(params => console.log(params)),
-        switchMap(params => {
-          return this.searchService.searchRide(params['id']);
-        }),
-        tap(route => {
-          const steps: number = route.path.length;
-          this.rideId = route.rideId;
-          this.departureDate = route.schedule.segments[0].time[0];
-          this.arrivalDate = route.schedule.segments[steps - 2].time[1];
-          this.carriages = route.carriages.map((el: string) => this.getCarriageData(el));
-          this.tripRoute = route;
-          console.log(route);
-        }),
-        takeUntilDestroyed(this.destroyRef),
-      )
-      .subscribe();
-
-    // from & to
-    this.route.queryParams
-      .pipe(
-        filter(params => params['from'] && params['to']),
-        map(params => ({
-          from: parseInt(params['from']),
-          to: parseInt(params['to']),
-        })),
-        switchMap(params =>
-          combineLatest([
-            this.stationsService.getStationById(params.from),
-            this.stationsService.getStationById(params.to),
-          ]),
-        ),
-        tap(res => {
-          this.stationFrom = { stationId: res[0]?.id, cityName: res[0]?.city };
-          this.stationTo = { stationId: res[1]?.id, cityName: res[1]?.city };
-        }),
-        takeUntilDestroyed(this.destroyRef),
-      )
-      .subscribe();
+    this.getData();
   }
 
   modal = signal<boolean>(false);
