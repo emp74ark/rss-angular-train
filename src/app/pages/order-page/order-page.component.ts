8a87c6183e34a0ab787e10ae13a82e11520f3272
Andrei Yurkouski <andrei.yurkouski@rapidfunnel.com>
2024-09-02 12:23:38 +0200

    feat: dummy order component

--- src/app/pages/order-page/order-page.component.ts
+++ src/app/pages/order-page/order-page.component.ts
@@ -2,11 +2,14 @@ import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { OrderService } from '../../services/order.service';
 import { tap } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { AsyncPipe } from '@angular/common';
+import { OrderDetailsComponent } from '../../components/order-details/order-details.component';
+import { Order } from '../../models/order';
 
 @Component({
   selector: 'app-order-page',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, OrderDetailsComponent],
   templateUrl: './order-page.component.html',
   styleUrl: './order-page.component.scss',
 })
@@ -14,13 +17,27 @@ export class OrderPageComponent implements OnInit {
   orderService = inject(OrderService);
   destroyRef = inject(DestroyRef);
 
-  ngOnInit() {
+  orders: Order[] = [];
+
+  getAllOrders() {
     this.orderService
-      .getOrders()
+      .getOrders(true)
       .pipe(
-        tap(orders => console.log(orders)),
+        tap(orders => {
+          if (orders) {
+            this.orders = orders;
+          }
+        }),
         takeUntilDestroyed(this.destroyRef),
       )
       .subscribe();
+  }
+
+  ngOnInit() {
+    this.getAllOrders();
+  }
+
+  onOrderListChanged() {
+    this.getAllOrders();
   }
 }
