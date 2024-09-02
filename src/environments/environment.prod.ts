60ac0df45b9e038c383b2911855690cbd9a095f6
Andrei Yurkouski <andrei.yurkouski@rapidfunnel.com>
2024-09-02 23:16:45 +0200

    feat: update env files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ import { Environment } from './backend';
 export const environment: Environment = {
   production: true,
   base_url: 'https://emp74ark.github.io/rss-angular-train',
+  geo_api_url: 'https://maps.googleapis.com',
   geo_api_key: '',
 };
