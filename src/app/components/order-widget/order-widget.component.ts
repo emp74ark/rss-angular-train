314a4c9df110dd5306d32ffb1e5630f7dcc4fa6e
Andrei Yurkouski <emp74ark@gmail.com>
2024-09-02 03:10:40 +0200

    feat: order the ride

--- src/app/components/order-widget/order-widget.component.ts
+++ src/app/components/order-widget/order-widget.component.ts
@@ -1,15 +1,27 @@
-import { Component, DestroyRef, inject, input } from '@angular/core';
+import { Component, DestroyRef, inject, input, signal } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatCard, MatCardActions, MatCardContent, MatCardTitle } from '@angular/material/card';
 import { Seat } from '../../models/carriage';
 import { CurrencyPipe } from '@angular/common';
 import { OrderService } from '../../services/order.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { switchMap, tap } from 'rxjs';
+import { ModalWindowComponent } from '../modal-window/modal-window.component';
+import { RouteGraphComponent } from '../route-graph/route-graph.component';
 
 @Component({
   selector: 'app-order-widget',
   standalone: true,
-  imports: [MatButton, MatCard, MatCardActions, MatCardContent, MatCardTitle, CurrencyPipe],
+  imports: [
+    MatButton,
+    MatCard,
+    MatCardActions,
+    MatCardContent,
+    MatCardTitle,
+    CurrencyPipe,
+    ModalWindowComponent,
+    RouteGraphComponent,
+  ],
   templateUrl: './order-widget.component.html',
   styleUrl: './order-widget.component.scss',
 })
@@ -18,6 +30,8 @@ export class OrderWidgetComponent {
   rideId = input<number>();
   stationStart = input<number>();
   stationEnd = input<number>();
+
+  orderId = signal<number | undefined>(undefined);
 
   orderService = inject(OrderService);
   destroyRef = inject(DestroyRef);
@@ -35,8 +49,26 @@ export class OrderWidgetComponent {
           stationStart,
           stationEnd,
         })
-        .pipe(takeUntilDestroyed(this.destroyRef))
-        .subscribe(res => console.log('ORDER', res));
+        .pipe(
+          switchMap(() => this.orderService.$apiStatus),
+          tap(status => {
+            if (status?.success) {
+              this.modalMessage.set(`Order successfully created with}`);
+            } else {
+              this.modalMessage.set(status?.error as string);
+            }
+            this.modal.set(true);
+          }),
+          takeUntilDestroyed(this.destroyRef),
+        )
+        .subscribe();
     }
+  }
+
+  modal = signal<boolean>(false);
+  modalMessage = signal<string>('');
+
+  onModal() {
+    this.modal.set(!this.modal());
   }
 }
