b046d294c77f9b6230aa3e4e3a7d2860ab193ad3
ayaki-coder <sj.a.yakimovich@gmail.com>
2024-09-02 10:38:47 +0300

    feat: add opening graph

--- src/app/components/result-card/result-card.component.ts
+++ src/app/components/result-card/result-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, inject, Input, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { CurrencyPipe, DatePipe } from '@angular/common';
@@ -6,18 +6,29 @@ import { MatIconModule } from '@angular/material/icon';
 import { DurationPipe } from '../../pipes/duration.pipe';
 import { UserTrip } from '../../models/train';
 import { Router } from '@angular/router';
+import { ModalWindowComponent } from '../modal-window/modal-window.component';
+import { RouteGraphComponent } from '../route-graph/route-graph.component';
 
 @Component({
   selector: 'app-result-card',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, DatePipe, MatIconModule, CurrencyPipe, DurationPipe],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    DatePipe,
+    MatIconModule,
+    CurrencyPipe,
+    DurationPipe,
+    ModalWindowComponent,
+    RouteGraphComponent,
+  ],
   templateUrl: './result-card.component.html',
   styleUrl: './result-card.component.scss',
 })
 export class ResultCardComponent {
   @Input() trip: UserTrip;
-
-  router = inject(Router);
+  readonly modal = signal<boolean>(false);
+  private readonly router = inject(Router);
 
   onNavigateToTrip() {
     const rideId = this.trip.rideId.toString();
@@ -29,5 +40,14 @@ export class ResultCardComponent {
         to: to,
       },
     });
+  }
+
+  openGraph(event: Event) {
+    event.stopPropagation();
+    this.onModal();
+  }
+
+  onModal() {
+    this.modal.set(!this.modal());
   }
 }
