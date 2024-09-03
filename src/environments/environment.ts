9d10cc11d12cf97ac4e26ae4b1d5469d5c91d93d
Andrei Yurkouski <emp74ark@gmail.com>
2024-09-03 15:09:54 +0200

    feat: update dev to upstream

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,6 @@ import { Environment } from './backend';
 
 export const environment: Environment = {
   production: false,
-  base_url: '',
-  geo_api_url: '',
+  geo_api_url: 'http://5.180.181.88:4000',
   geo_api_key: '',
 };
