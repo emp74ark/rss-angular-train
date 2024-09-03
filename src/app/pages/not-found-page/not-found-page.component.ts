6c26fef81f9c9ac2d29ffa618f7d2ecc5a7aa5bb
siviavio4ka <siviavio4ka@gmail.com>
2024-09-03 20:09:10 +0300

    feat: add intarface for order cards

--- src/app/pages/not-found-page/not-found-page.component.ts
+++ src/app/pages/not-found-page/not-found-page.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { MatGridList, MatGridTile } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-not-found-page',
   standalone: true,
-  imports: [MatGridList, MatGridTile],
+  imports: [],
   templateUrl: './not-found-page.component.html',
   styleUrl: './not-found-page.component.scss',
 })
