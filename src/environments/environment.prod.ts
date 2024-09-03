3431dfa26ba3e6818847db50aca64905185b6bfe
Andrei Yurkouski <emp74ark@gmail.com>
2024-09-03 14:59:01 +0200

    feat: use own geo proxy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import { Environment } from './backend';
 
 export const environment: Environment = {
   production: true,
-  geo_api_url: 'http://5.180.181.88:8080',
+  geo_api_url: 'http://5.180.181.88:4000',
   geo_api_key: '',
 };
